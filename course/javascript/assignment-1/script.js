f84a8fba7c561f66f327157c5e474ba7c99db2ef
christophergidlof <christophergidlof@gmail.com>
2015-08-13 18:49:54 +0200

    Create script.js

---
+++ course/javascript/assignment-1/script.js
@@ -0,0 +1,10 @@
+var hello = "Hello";
+var name = "Name";
+var welcome = "Welcome to the";
+var appname = "ApplicationName";
+var app = "Application!";
+
+name = prompt("Type your name", name);
+appname = prompt("Type appname", appname);
+
+console.log(hello + ' ' + name + ' ' + welcome + ' ' + appname + ' ' + app);
